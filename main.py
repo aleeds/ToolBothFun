74b59d36793cf7d8508b15ac18d3161c2b2b489b
Isaac Thacker <ithacker@isaacs-air.trinity.local>
2017-01-20 18:47:45 -0600

    Progress on advancement function

--- main.py
+++ main.py
@@ -4,12 +4,29 @@ road conditions (other cars, new lanes, stopping at toll booths, etc)
 """
 
 
-def advancement(actions, road):
+def advancement(actions, laneSegment):
     for action in actions:
-        #todo
+        car = action[0]    
+        carAction = action[1]
+        if carAction.stopTime != 0:
+            laneSegment[car.lane_spot_index] = 0
+            nextIdx = car.lane_spot_index + carAction.speedChange          #####
+            if nextIdx > len(laneSegment.lane_spots):
+                nextIdx %= len(laneSegment.lane_spots)
+            if carAction.laneChange == -1:
+                laneSegment = laneSegment.lane_end[0]
+            elif carAction.laneChange == -1:
+                laneSegment = laneSegment.lane_end[1]
+            elif carAction.laneChange == -1:
+                laneSegment = laneSegment.lane_end[2]
+            laneSegment[nextIdx] = car
+        else:
+            carAction.stopTime -= 1
 
-roadPieces1 = []
-roadPieces2 = []
+
+
+roadPieces1 = []  #list of lane_segments
+roadPieces2 = []  #list of land_segments
 roads = (roadPieces1, roadPieces2)
 
 curRoad = 0
@@ -19,6 +36,6 @@ for x in range(0, timeDuration):
     for piece in roads[curRoad]:
         for spot in piece.lane_spots:
             if spot != 0:
-                actions.append(spot.move(roads[1-curRoad]))
+                actions.append([spot, spot.move(piece)])
     advancement(actions, boards[1-curRoad])
     curRoad = 1 - curRoad 
